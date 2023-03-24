95a7bc40b647adbfa96b17cc6149dcf3070ff679
Gonzalo Aruquipa <gonzao777info@gmail.com>
2023-03-24 19:07:22 -0400

    views propiedad by id

--- controllers/propiedadesController.js
+++ controllers/propiedadesController.js
@@ -1,19 +1,19 @@
 import { validationResult } from "express-validator";
+import {unlink} from "node:fs/promises"
 import { Categoria, Precio, Propiedad } from "../models/index.js";
 const admin = async (req, res) => {
   const { id } = req.usuario;
-  const propiedades = await Propiedad.findAll({ 
-    where: 
-    { usuarioId: id
-    },
+  const propiedades = await Propiedad.findAll({
+    where: { usuarioId: id },
     include: [
-      {model: Categoria, as:"categoria"},
-      {model: Precio, as: "precio"}
-    ]
-   });
+      { model: Categoria, as: "categoria" },
+      { model: Precio, as: "precio" },
+    ],
+  });
 
   res.render("propiedades/admin", {
     pagina: "Mis Propiedades",
+    csrfToken: req.csrfToken(),
     propiedades,
   });
 };
@@ -134,4 +134,133 @@ const almacenarImagen = async (req, res, next) => {
   }
 };
 
-export { admin, crear, guardar, agregarImagen, almacenarImagen };
+const editar = async (req, res) => {
+  const { id } = req.params;
+  const propiedad = await Propiedad.findByPk(id);
+  if (!propiedad) {
+    return res.redirect("/mis-propiedades");
+  }
+  if (propiedad.usuarioId.toString !== req.usuario.id.toString) {
+    return res.redirect("/mis-propiedades");
+  }
+  const categorias = await Categoria.findAll();
+  const precios = await Precio.findAll();
+
+  res.render("propiedades/editar", {
+    pagina: `Editar Propiedad: ${propiedad.titulo}`,
+    csrfToken: req.csrfToken(),
+    categorias: categorias,
+    precios: precios,
+    datos: propiedad,
+  });
+};
+const guardarCambios = async (req, res) => {
+  let result = validationResult(req);
+
+  if (!result.isEmpty()) {
+    const categorias = await Categoria.findAll();
+    const precios = await Precio.findAll();
+    return res.render("propiedades/editar", {
+      pagina: `Editar Propiedad`,
+      csrfToken: req.csrfToken(),
+      errores: result.array(),
+      categorias: categorias,
+      precios: precios,
+      datos: req.body,
+    });
+  }
+
+  const { id } = req.params;
+  const propiedad = await Propiedad.findByPk(id);
+  if (!propiedad) {
+    return res.redirect("/mis-propiedades");
+  }
+  if (propiedad.usuarioId.toString !== req.usuario.id.toString) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  try {
+    const {
+      titulo,
+      descripcion,
+      categoria: categoriaId,
+      precio: precioId,
+      habitaciones,
+      estacionamiento,
+      wc,
+      calle,
+      lat,
+      lng,
+    } = req.body;
+    propiedad.set({
+      titulo,
+      descripcion,
+      categoriaId,
+      precioId,
+      habitaciones,
+      estacionamiento,
+      wc,
+      calle,
+      lat,
+      lng,
+    });
+    await propiedad.save();
+    res.redirect("/mis-propiedades");
+  } catch (error) {
+    console.log(error);
+  }
+};
+const eliminar = async (req, res) => {
+  const { id } = req.params;
+
+  const propiedad = await Propiedad.findByPk(id);
+
+  if (!propiedad) {
+    return res.redirect("/mis-propiedades");
+  }
+  if (propiedad.publicado) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  if (propiedad.usuarioId.toString() !== req.usuario.id.toString()) {
+    return res.redirect("/mis-propiedades");
+  }
+
+  await unlink(`public/uploads/${propiedad.imagen}`)
+  await propiedad.destroy()
+  res.redirect("/mis-propiedades")
+};
+
+const mostrarPropiedad = async(req, res) => {
+
+  const {id} = req.params
+
+
+  const propiedad = await Propiedad.findByPk(id, {
+    include: [
+      { model: Categoria, as: "categoria" },
+      { model: Precio, as: "precio" },
+    ],
+  });
+  if(!propiedad){
+    return res.redirect("/404")
+  }
+  res.render("propiedades/mostrar", {
+    propiedad,
+    pagina: propiedad.titulo,
+
+
+  })
+}
+
+export {
+  admin,
+  crear,
+  guardar,
+  agregarImagen,
+  almacenarImagen,
+  editar,
+  guardarCambios,
+  eliminar,
+  mostrarPropiedad,
+};
--- routes/propiedadesRoutes.js
+++ routes/propiedadesRoutes.js
@@ -6,6 +6,10 @@ import {
   guardar,
   agregarImagen,
   almacenarImagen,
+  editar,
+  guardarCambios,
+  eliminar,
+  mostrarPropiedad
 } from "../controllers/propiedadesController.js";
 import protegerRuta from "../middleware/protegerRuta.js";
 import upload from "../middleware/subirImagen.js";
@@ -43,4 +47,30 @@ router.post(
   upload.single("imagen"),
   almacenarImagen
 );
+router.get("/propiedades/editar/:id", protegerRuta, editar);
+
+router.post(
+  "/propiedades/editar/:id",
+  protegerRuta,
+  body("titulo").notEmpty().withMessage("El título es obligatorio"),
+  body("descripcion")
+    .notEmpty()
+    .withMessage("La descripción no puede ir vacía")
+    .isLength({ max: 200 })
+    .withMessage("La descripción es muy larga"),
+  body("categoria").isNumeric().withMessage("Selecciona una categoría"),
+  body("precio").isNumeric().withMessage("Selecciona un precio"),
+  body("habitaciones")
+    .isNumeric()
+    .withMessage("Selecciona la cantidad de habitaciones"),
+  body("estacionamiento")
+    .isNumeric()
+    .withMessage("Selecciona la cantidad de estacionamientos"),
+  body("wc").isNumeric().withMessage("Selecciona la cantidad de baños"),
+  body("lat").notEmpty().withMessage("Ubica la propiedad en el mapa"),
+
+  guardarCambios
+);
+router.post("/propiedades/eliminar/:id", protegerRuta, eliminar)
+router.get("/propiedad/:id", mostrarPropiedad)
 export default router;
