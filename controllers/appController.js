0672fb7b8f612e8e82ef5205ad0024eebfecbd4a
Gonzalo Aruquipa <gonzao777info@gmail.com>
2023-03-30 10:20:39 -0400

    clear map and show filter propiedades

--- controllers/appController.js
+++ controllers/appController.js
@@ -3,11 +3,29 @@ const inicio =  async(req, res) => {
 
   const precios = await Precio.findAll({raw: true});
   const categorias = await Categoria.findAll({raw: true});
+  const casas = await Propiedad.findAll({
+    limit: 3,
+    where:{
+      categoriaId: 1
+    },
+    include: [{model: Precio, as :"precio"}],
+    order: [["createdAt", "DESC"]]
+  })
+  const departamentos = await Propiedad.findAll({
+    limit: 3,
+    where:{
+      categoriaId: 2
+    },
+    include: [{model: Precio, as :"precio"}],
+    order: [["createdAt", "DESC"]]
+  })
   
   res.render("inicio", {
     pagina: "Inicio",
     categorias,
     precios,
+    casas,
+    departamentos,
   });
 };
 
--- src/js/mapaInicio.js
+++ src/js/mapaInicio.js
@@ -1,51 +1,70 @@
 import axios from "axios";
-(function(){
-
-  
+(function () {
   const lat = -16.503831;
   const lng = -68.1317191;
   const mapa = L.map("mapa-inicio").setView([lat, lng], 13);
 
-  let markers = new L.FeatureGroup().addTo(mapa)
+  let markers = new L.FeatureGroup().addTo(mapa);
+
+  let propiedades = [];
+
+  const filtros = {
+    categoria: "",
+    precio: "",
+  };
+  const categoriasSelect = document.querySelector("#categorias");
+  const preciosSelect = document.querySelector("#precios");
 
   L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
       '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(mapa);
 
-  const obtenerPropiedades = async () => {
+  categoriasSelect.addEventListener("change", (e) => {
+    filtros.categoria = +e.target.value;
+    filtrarPropiedades();
+  });
 
+  preciosSelect.addEventListener("change", (e) => {
+    filtros.precio = +e.target.value;
+    filtrarPropiedades();
+  });
+
+  const obtenerPropiedades = async () => {
     try {
-      const url = "/api/propiedades"
-      const respuesta = await axios.get(url)
-      const propiedades = respuesta.data;
+      const url = "/api/propiedades";
+      const respuesta = await axios.get(url);
+      propiedades = respuesta.data;
 
       mostrarPropiedades(propiedades);
-
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
+  };
 
   const mostrarPropiedades = (propiedades) => {
-    propiedades.forEach(propiedad => {
+
+    markers.clearLayers()
+
+    propiedades.forEach((propiedad) => {
       const marker = new L.marker([propiedad?.lat, propiedad?.lng], {
-        autoPan:true
-      })
-      .addTo(mapa)
-      .bindPopup(`
+        autoPan: true,
+      }).addTo(mapa).bindPopup(`
         <p class="text-indigo-600 font-bold">${propiedad?.categoria.nombre}</p>
         <h1 class="text-xl font-extrabold uppercase my-2">${propiedad?.titulo}</h1>
         <img src="/uploads/${propiedad?.imagen}" alt="${propiedad.titulo}">
         <p class="text-gray-600 font-bold">${propiedad?.precio.nombre}</p>
         <a href="/propiedad/${propiedad.id}" class="bg-indigo-600 block p-2 text-center font-bold uppercase ">Ver Propiedad</a>
       
-      `)
+      `);
 
-      markers.addLayer(marker)
-    })
-  }
+      markers.addLayer(marker);
+    });
+  };
+  const filtrarPropiedades = () => {
+    const resultado = propiedades.filter(propiedad => filtros.categoria? propiedad.categoriaId === filtros.categoria : propiedad).filter(propiedad => filtros.precio? propiedad.precioId === filtros.precio : propiedad)
+    mostrarPropiedades(resultado)
+  };
 
   obtenerPropiedades();
-
-})()
+})();
